dc8dad08a4d2f6d7839f97a7e41681644b44005d
Christian Leo-Pernold <christian.leo@akwien.at>
2021-12-11 19:42:52 +0100

    WIP.

---
+++ resources/views/livewire/day04.blade.php
@@ -0,0 +1,7 @@
+<x-day
+    day="04"
+    :codeOne="$codeOne"
+    :codeTwo="$codeTwo"
+    :solutionOne="$solutionOne"
+    :solutionTwo="$solutionTwo"
+/>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Livewire\Day01;
 use App\Http\Livewire\Day02;
 use App\Http\Livewire\Day03;
+use App\Http\Livewire\Day04;
 use App\Http\Livewire\ShowCalendar;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,4 @@ Route::get('/', ShowCalendar::class)->name('home');
 Route::get('days/1', Day01::class);
 Route::get('days/2', Day02::class);
 Route::get('days/3', Day03::class);
+Route::get('days/4', Day04::class);
