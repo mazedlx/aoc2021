5f6d3d22bec2c300ee04867b4e13a746179883ef
Christian Leo-Pernold <christian.leo@akwien.at>
2021-12-12 10:03:33 +0100

    WIP.

--- resources/views/livewire/show-calendar.blade.php
+++ resources/views/livewire/show-calendar.blade.php
@@ -4,10 +4,16 @@
     <div class="grid grid-cols-5 gap-x-4 gap-y-6">
         @foreach (range(1,25) as $day)
         <div>
+        @if (class_exists('App\Http\Livewire\Day' . str_pad($day, 2, '0', STR_PAD_LEFT)))
             <a
                 href="days/{{ $day }}"
                 class="inline-flex items-center justify-center w-full px-4 py-8 border-2 border-gray hover:text-light-green text-green"
             >Day {{ $day }}</a>
+        @else
+        <div class="inline-flex items-center justify-center w-full px-4 py-8 border-2 border-gray text-gray">
+            Day {{ $day }}
+        </div>
+        @endif
         </div>
         @endforeach
     </div>
